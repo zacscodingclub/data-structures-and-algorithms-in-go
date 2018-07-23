a25d1f1ee146bd8d671c2508ac74e95ebc96b598
zacscodingclub <zbaston@gmail.com>
2018-07-22 18:07:21 -0700

    finish chapter 2

---
+++ chapter-02-arrays/02_notes.md
@@ -0,0 +1,33 @@
+# Arrays
+
+## Summary
+
+- Unordered arrays offer fast insertion but slow searching and deletion.
+- Wrapping an array in a class protects the array from being inadvertently altered.
+- A class interace is composed of the methods (and occaisionally fields) that the class user can access.
+- A class interface can be designed to make things simple for the class user.
+- A binary search can be applied to an ordered array.
+- The logarithm to the base B of a number A is (roughly) the number of times you can divide A by B before the result is 1.
+- Linear searches require time proportional to the number of items in an array.
+- Binary searches require time proportional to the logarithm of the number of items.
+- Big O notation provides a convenient way to compare the speed of algorithms.
+- An algorithm that runs in O(1) time is the best, O(log N) is good, O(N) is fair, and O(N^2) is bad.
+
+## Questions
+
+1.  d
+2.  true
+3.  b
+4.  false
+5.  new
+6.  b.... d
+7.  interface
+8.  d
+9.  exponent
+10. 3
+11. b
+12. 6
+13. false
+14. a
+15. constant
+16. objects
---
+++ chapter-02-arrays/array.go
@@ -0,0 +1,135 @@
+package main
+
+import "fmt"
+
+type Array struct {
+	values []int
+	length int
+}
+
+func NewArray(size int) *Array {
+	a := make([]int, size, size)
+	arr := Array{values: a}
+	return &arr
+}
+
+func (a *Array) Insert(value int) {
+	a.values[a.Size()] = value
+	a.length += 1
+}
+
+func (a *Array) GetMax() int {
+	var j int
+	max := -1
+	for j = 0; j < a.Size(); j++ {
+		if a.values[j] > max {
+			max = a.values[j]
+		}
+	}
+	return max
+}
+
+func (a *Array) NoDups() []int {
+	filtered := []int{}
+	found := map[int]int{}
+
+	for i := 0; i < a.Size(); i++ {
+		if _, ok := found[a.values[i]]; !ok {
+			found[a.values[i]] = 1
+			filtered = append(filtered, a.values[i])
+		}
+	}
+
+	return filtered
+}
+
+func (a *Array) RemoveMax() {
+	var j, maxIndex int
+	max := -1
+	for j = 0; j < a.Size(); j++ {
+		if a.values[j] > max {
+			max = a.values[j]
+			maxIndex = j
+		}
+	}
+	if max != -1 {
+		for k := maxIndex; k < a.Size(); k++ {
+			a.values[k] = a.values[k+1]
+		}
+		a.length -= 1
+	}
+
+}
+
+func (a *Array) Find(key int) bool {
+	var j int
+	for j = 0; j < a.Size(); j++ {
+		if a.values[j] == key {
+			break
+		}
+	}
+	if j == a.Size() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (a *Array) Delete(key int) bool {
+	var j int
+	for j = 0; j < a.Size(); j++ {
+		if a.values[j] == key {
+			break
+		}
+	}
+	if j == a.Size() {
+		return false
+	} else {
+		for k := j; k < a.Size(); k++ {
+			a.values[k] = a.values[k+1]
+		}
+		a.length -= 1
+
+		return true
+	}
+}
+
+func (a *Array) Display() {
+	for i := 0; i < a.Size(); i++ {
+		fmt.Printf("%v\n", a.values[i])
+	}
+}
+
+func (a *Array) Size() int {
+	return a.length
+}
+
+func ArrayRun() {
+	a := NewArray(100)
+
+	a.Insert(77)
+	a.Insert(99)
+	a.Insert(44)
+	a.Insert(12)
+	a.Insert(12)
+	a.Insert(55)
+	a.Insert(77)
+	a.Display()
+
+	searchKeyOne := 36
+	if a.Find(searchKeyOne) {
+		fmt.Printf("Found: %v\n", searchKeyOne)
+	} else {
+		fmt.Printf("Didn't find: %v\n", searchKeyOne)
+	}
+	searchKeyTwo := 12
+	if a.Find(searchKeyTwo) {
+		fmt.Printf("Found: %v\n", searchKeyTwo)
+	} else {
+		fmt.Printf("Didn't find: %v\n", searchKeyTwo)
+	}
+
+	a.Display()
+
+	fmt.Printf("%v\n", a.NoDups())
+}
